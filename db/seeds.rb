8f8c954d8a87b02defc7b36da93ae0323dd06ba9
Marcia-Free <MarciaFreeman2011@gmail.com>
2020-11-10 13:20:54 -0600

    Add Dogs to Seeds file - Belgian_Malinois and all after (8 total)

---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+#Dog Breeds	- "Breed Name"
+#Activity Level	- "High, Medium, Low"
+#Kid Friendly? - "Yes, No"
+#Dog Size	- "Large, Medium, Small"
+#Hypoallergenic? - "Yes, No"
+
+Belgian_Malinois = Breed.create(breed: "Belgian Malinois", activity_level:	"High",	kid_friendly: "No", dog_size: "Medium", hypoallergenic: "No")
+Shiba_Inu = Breed.create(breed: "Shiba Inu", activity_level:	"Medium",	kid_friendly: "Yes", dog_size: "Small", hypoallergenic: "No")
+Chesapeake_Bay_Retriever = Breed.create(breed: "Chesapeake Bay Retriever", activity_level:	"High",	kid_friendly: "Yes", dog_size: "Medium", hypoallergenic: "No")
+Bichon_Frise = Breed.create(breed: "Bichon Frise", activity_level:	"Medium",	kid_friendly: "Yes", dog_size: "Small", hypoallergenic: "Yes")
+Akita = Breed.create(breed: "Akita", activity_level:	"Medium",	kid_friendly: "No", dog_size: "Large", hypoallergenic: "No")
+St_Bernard = Breed.create(breed: "St. Bernards", activity_level:	"Medium",	kid_friendly: "Yes", dog_size: "Large", hypoallergenic: "No")
+Bloodhound = Breed.create(breed: "Bloodhound", activity_level:	"Medium",	kid_friendly: "Yes", dog_size: "Large", hypoallergenic: "No")
+Portuguese_Water_Dog = Breed.create(breed: "Portuguese Water Dog", activity_level:	"High",	kid_friendly: "No", dog_size: "Medium", hypoallergenic: "Yes")
