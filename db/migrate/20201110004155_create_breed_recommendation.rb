7437696908753084ce502c9d90be9bec74fc721e
https://learn.co/DenaNeuman <denaweiss5@gmail.com>
2020-11-10 11:00:54 -0600

    Adjusted class

--- db/migrate/20201110004155_create_breed_recommendation.rb
+++ db/migrate/20201110004155_create_breed_recommendation.rb
@@ -1,6 +1,6 @@
 class CreateBreedRecommendation < ActiveRecord::Migration[6.0]
   def change
-    create_table :pet_recommendations do |t|
+    create_table :breed_recommendations do |t|
       t.string :breed_name
       t.string :activity_level
       t.string :dog_size
