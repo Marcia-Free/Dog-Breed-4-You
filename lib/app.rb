09d08e2b25935ee2e1e52bb44740bd161ec11432
https://learn.co/DenaNeuman <denaweiss5@gmail.com>
2020-11-11 13:12:45 -0600

    finished user status

--- lib/app.rb
+++ lib/app.rb
@@ -3,9 +3,14 @@ class App
     def user_status
         prompt = TTY::Prompt.new
         input = prompt.select("What is your current status?", %w(New_User Existing_User))
-        #user_input = gets.chomp
 
-        #binding.pry
+        system "clear"
+        
+        if input == "New_User"
+            User.create_new_user
+        elsif input == "Existing_User"
+            User.find_existing_user
+        end
     end
 
 
--- lib/user.rb
+++ lib/user.rb
@@ -2,5 +2,38 @@ class User < ActiveRecord::Base
     has_many :users_breeds
     has_many :breeds, through: :users_breeds
 
+    def self.create_new_user  # ==> if input == "New_User"
+        puts "Please create your username:"
+        user_name = gets.chomp
+
+        system "clear"
+
+        if User.find_by(name: user_name)
+            puts "Uh-Oh! Looks like that username is already taken! Please create a new one."
+            create_new_user
+        else
+            current_user = User.create(name: user_name)
+            puts "Username #{current_user.name} was created successfully! Welcome #{current_user.name}!"
+        end
+        current_user
+    end
+
+    def self.find_existing_user # ==> if input == "Existing_User"
+        puts "Please enter your username:"
+        user_name = gets.chomp
+        current_user = User.find_by(name: user_name)
+
+        if User.exists?(name: user_name)
+            puts "Welcome back, #{current_user.name}!"
+        
+        else
+            puts "Sorry! Username does not exist! Please enter a different username."
+            find_existing_user
+            #=>option to create new one or enter different username
+        end
+        current_user
+
+    end
+
 
 end
