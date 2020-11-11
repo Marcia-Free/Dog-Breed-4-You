9b56f09ce51a52b3ed1b7ae4615349913d904146
Marcia-Free <MarciaFreeman2011@gmail.com>
2020-11-10 18:09:26 -0600

    Welcome Screen Done

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,25 @@
 require_relative '../config/environment'
 
-App.new.welcome
+def display_title
+    font = TTY::Font.new("3d")
+    pastel = Pastel.new
 
-puts "HELLO WORLD"
+    puts pastel.white(font.write("Welcome"))
+    puts pastel.white(font.write("To"))
+    sleep(2,)
+
+    puts pastel.magenta(font.write("Dog"))
+    puts pastel.magenta(font.write("Breed"))
+    puts pastel.bright_magenta(font.write("4"))
+    puts pastel.bright_magenta(font.write("You"))
+    sleep(5,)
+end
+
+display_title
+system "clear"
+
+App.new.user_status
+
+
+# prompt = TTY::Prompt.new
+# input = prompt.select("Please setup your account", %w(Make_an_account Look_at_your_recommendations Goodbye))
