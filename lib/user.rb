aa2fb4846ee3128d545937d251582e4052e41955
https://learn.co/DenaNeuman <denaweiss5@gmail.com>
2020-11-11 13:28:39 -0600

    system cleared for user status

--- lib/user.rb
+++ lib/user.rb
@@ -9,10 +9,12 @@ class User < ActiveRecord::Base
         system "clear"
 
         if User.find_by(name: user_name)
+            system "clear"
             puts "Uh-Oh! Looks like that username is already taken! Please create a new one."
             create_new_user
         else
             current_user = User.create(name: user_name)
+            system "clear"
             puts "Username #{current_user.name} was created successfully! Welcome #{current_user.name}!"
         end
         current_user
@@ -24,15 +26,27 @@ class User < ActiveRecord::Base
         current_user = User.find_by(name: user_name)
 
         if User.exists?(name: user_name)
+            system "clear"
             puts "Welcome back, #{current_user.name}!"
         
         else
-            puts "Sorry! Username does not exist! Please enter a different username."
-            find_existing_user
+            system "clear"
+            # puts "Sorry! Username does not exist! Please enter a different username."
+            # find_existing_user
             #=>option to create new one or enter different username
+
+            prompt = TTY::Prompt.new
+            input = prompt.select("Looks like that username does not exist! Would you like to create a new username or try again?", %w(New_Username Try_Again))
+
+            if input == "New_Username"
+                system "clear"
+                self.create_new_user
+            elsif input == "Try_Again"
+                system "clear"
+                self.find_existing_user
+            end
         end
         current_user
-
     end
 
 
